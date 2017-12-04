31c6d5da00fbf7223837443035cbcecdd716a979
Anton D <rus-21region@mail.ru>
2017-12-04 12:45:33 +0300

    Modules 3.11-3.15

--- app/index.js
+++ app/index.js
@@ -1,15 +1,25 @@
 import React from 'react';
 import EStyleSheet from 'react-native-extended-stylesheet';
 
-import Home from './screens/Home';
+// import Home from './screens/Home';
+// import CurrencyList from './screens/CurrencyList';
+// import Options from './screens/Options';
+import Themes from './screens/Themes';
 
 EStyleSheet.build({
   $primaryBlue: '#4F6D7A',
+  $primaryOrange: '#D57A66',
+  $primaryGreen: '#00BD9D',
+  $primaryPurple: '#9E768F',
 
   $white: '#FFF',
   $lightGray: '#F0F0F0',
   $border: '#979797',
   $inputText: '#797979',
+  $darkText: '#343434',
 });
 
-export default () => <Home />;
+// export default () => <Home />;
+// export default () => <CurrencyList />;
+// export default () => <Options />;
+export default () => <Themes />;
